4a261153ccd094b2a6959da294dbcc6a85f046b0
TijaniDevIt <tijanidevit@yahoo.com>
2021-09-15 01:26:05 +0100

    CourseStudentsMigration

---
+++ database/migrations/2021_09_14_235635_create_course_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_students', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id');
+            $table->unsignedBigInteger('student_id');
+            $table->Integer('current_points')->default(0);
+            $table->Integer('highest_points')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_students');
+    }
+}
